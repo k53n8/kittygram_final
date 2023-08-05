e0b2bac63fe9301219e0c02d9d5c4de0c2291b47
Олег Иванов <ksengod@yandex.ru>
2023-08-05 18:51:40 +0500

    db fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'kittygram_db'),
         'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
